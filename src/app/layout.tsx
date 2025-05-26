ad4a7a308e788767bd3dc2793dbeed53ae8830a1
KR Shanto <krshanto2005@gmail.com>
2025-05-26 16:24:13 +0600

    feat(layout): update font in layout component
    - Replace Inter font with Poppins font for improved typography
    - Specify font weights for Poppins to enhance text styling
    
    Key changes:
    - 🎨 Enhance visual consistency with updated font choice
    - 🔤 Ensure proper font weights are applied for better readability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Template from "../components/Template";
 import { getUser } from "@/lib/auth";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "MPOS Police Plaza",
@@ -20,7 +23,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <Template user={user}>{children}</Template>
       </body>
     </html>
